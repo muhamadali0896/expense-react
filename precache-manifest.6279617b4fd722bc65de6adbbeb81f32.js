4f6ea772cccf44876d5c5da2c4c3740ecf1c10bf
muhamadali0896 <72602422+muhamadali0896@users.noreply.github.com>
2022-08-19 23:18:55 +0530

    Updates

---
+++ precache-manifest.6279617b4fd722bc65de6adbbeb81f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463dd0d0fd7ac94d4c0d83113149c6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae5b7a13264cd5d19aa",
+    "url": "/static/css/2.7ac39a5d.chunk.css"
+  },
+  {
+    "revision": "abd55a81a9386431d6ea",
+    "url": "/static/css/main.4d4eb2ea.chunk.css"
+  },
+  {
+    "revision": "6ae5b7a13264cd5d19aa",
+    "url": "/static/js/2.fa8636d4.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/static/js/2.fa8636d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd55a81a9386431d6ea",
+    "url": "/static/js/main.ee8e844a.chunk.js"
+  },
+  {
+    "revision": "f31b34e9a7a60b9e51c0",
+    "url": "/static/js/runtime-main.e901c6d6.js"
+  }
+]);
